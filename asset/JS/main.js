5d9086085b45f1a8e804d93ef4ff83373fa39338
Markiza666 <3ggjs24erikma@folkuniversitetet.nu>
2024-12-08 20:32:13 +0100

    Lägger uppdaterade kommentarer

--- asset/JS/main.js
+++ asset/JS/main.js
@@ -4,18 +4,22 @@ import { presentInfo } from './modalFunctions.js';
 import { planetColors } from './variables.js';
 
 
-//Kör funktion för att hämta data genom API
+// Kör funktion för att hämta data genom API
 getKeyAndSolarSystemData();
 
 
-
+// Funktion för att lägga alla händelselyssnare på solen och planeterna
 const initiatePage = () => {
+    // Nollställer en räknare (c = counter)
     let c = 0;
+    // Loopar igenom arrayen för planeternas färger för att ta ut planetnamnet (key)
     for (const key in planetColors) {
-        let z = c;
+        let z = c;      // Sparar nuvarande värde av "c" till en ny variabel "z" för att kunna skilja varje lyssnare åt
+        // Kontrollerar att object har både värde och nyckel 
         if (Object.prototype.hasOwnProperty.call(planetColors, key)) {
-            const element = planetColors[key];
-            domObjects[key].addEventListener('click', () => {presentInfo(systemData[z], planetColors[key])});
+            domObjects[key].addEventListener('click', () => {
+                presentInfo(systemData[z], planetColors[key])       // Lägger en händelselyssnare på solen och planeterna
+            });
             c++;
         }
     }
